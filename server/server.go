8dd850dc55c60fdc57f558679e40ae1dc00653f7
VigLinat <prokhorov1099@gmail.com>
2024-10-08 18:19:30 +0300

    Single room for all connections.

--- server/server.go
+++ server/server.go
@@ -2,27 +2,46 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
 	"time"
+    "github.com/google/uuid"
 )
 
 var hostPort = "50160"
 var hostAddr = "localhost"
 var peerChan chan string = make(chan string)
 
-var rooms map[int]message
+var room chan *Message // One common room for now
 
-type client struct {
-    connection net.Conn
-    name string
+type Client struct {
+    Conn net.Conn
+    // Name string // TODO: implement later
+    uuid string // SHA-1
 }
 
-type message struct {
-    user string
-    content string
+// Read reads data from Client connection to room
+func (client *Client) Read(room chan <- *Message) {
+    buffer := make([]byte, 2048)
+    for {
+        if _, err := client.Conn.Read(buffer); err != nil {
+            message := &Message{client, buffer} 
+            room <- message
+        }
+    }
+}
+
+// Write writes data from room to Client connection
+func (client *Client) Write(room <-chan *Message) {
+    for message := range room {
+        client.Conn.Write(message.Content)
+    }
+}
+
+type Message struct {
+    *Client
+    Content []byte
 }
 
 func main() {
@@ -32,52 +51,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    go listenForClients(listener)
-}
-
-func listenForClients(listener net.Listener) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
+        logConnReceived(conn)
         handleNewClient(conn)
 	}
 }
 
-func listenToClient(client net.Conn) <-chan string {
-    res := make(chan string)
-    go func(conn net.Conn) {
-        buff := make([]byte, 2048)
-        for {
-            if n, err := conn.Read(buff); err != nil {
-                res <- string(buff[:n])
-            }
-        }
-    }(client)
-    return res
-}
-
 func handleNewClient(c net.Conn) {
     logConnReceived(c)
-    connectedClients[c.LocalAddr().String()] = c
-}
-
-func handleConn(c net.Conn) {
-	fmt.Fprintf(os.Stdout, "MESSAGE [%s] Received byte sequence:", time.Now().Format(time.TimeOnly))
-	io.Copy(os.Stdout, c) // TEMPORARY
-	fmt.Fprintln(os.Stdout)
-
-	logConnClosing(c)
+    clientUUID := uuid.NewString()
+    newClient := &Client{c, clientUUID}
+    go newClient.Read(room)
+    go newClient.Write(room)
 }
 
 func logConnReceived(c net.Conn) {
 	fmt.Fprintf(os.Stderr, "LOG [%s] Received connection: %s\n", time.Now().Format(time.TimeOnly), c.RemoteAddr().String())
-}
-
-func logConnClosing(c net.Conn) {
-	fmt.Fprintf(os.Stderr, "LOG [%s] Closing connection %s\n", time.Now().Format(time.TimeOnly), c.RemoteAddr().String())
 }
 
 // TODO: use built-in flag parsing
